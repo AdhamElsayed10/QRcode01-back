6310d15191fc608a4860b5357616be7aafdf709a
Adham El Sayed <adhamadh445@gmail.com>
2026-06-22 21:29:11 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -63,7 +63,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5050;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
